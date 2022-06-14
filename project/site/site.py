7e4306263a5d853f407935cb79f6b62c02cb8d45
Jakub Hlavačka <43497822+JKBGIT1@users.noreply.github.com>
2022-06-14 15:53:42 +0200

    forms test passing

--- project/site/site.py
+++ project/site/site.py
@@ -1,23 +1,21 @@
-from flask import Blueprint, render_template, request, url_for, flash, redirect
+from flask import Blueprint, render_template, request, flash, redirect, abort
 
 site = Blueprint('site', __name__)
 
 @site.route('/', methods=['GET', 'POST'])
 def index():
-    message = None
-
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
 
         if not password or not username or username == '' or password == '':
             flash('Vyplnte prihlásovacia údaje.', category='error')
+            return render_template('login.html'), 400
         else:
             return redirect('/home')
 
-    return render_template('login.html', message=message)
-
+    return render_template('login.html'), 200
 
 @site.route('/home', methods=['GET', 'POST'])
 def home():
-    return render_template('home.html')
+    return render_template('home.html'), 200
--- tests/functional/test_form.py
+++ tests/functional/test_form.py
@@ -1,9 +1,56 @@
+import json
+
 def test_login_form(test_client):
     response = test_client.get('/')
     assert response.status_code == 200
 
-    page_content = response.data
+    page_content = response.text
 
     labels = ['Používateľské meno', 'Heslo']
     for label in labels:
         assert label in page_content
+
+def test_sunny_day_submit(test_client):
+    data = {
+        "username": "username",
+        "password": "password"
+    }
+
+    response = test_client.post('/', data=data, follow_redirects=True)
+
+    assert len(response.history) == 1 # there has to be one redirect
+    assert response.status_code == 200
+    assert response.request.path == '/home'
+
+def test_not_filled_user_data(test_client):
+    data = {
+        "username": "",
+        "password": ""
+    }
+
+    response = test_client.post('/', data=data)
+
+    assert response.status_code == 400
+    assert 'Vyplnte prihlásovacia údaje.' in response.text
+
+def test_not_filled_username(test_client):
+    data = {
+        "username": "",
+        "password": "password"
+    }
+
+    response = test_client.post('/', data=data)
+
+    assert response.status_code == 400
+    assert 'Vyplnte prihlásovacia údaje.' in response.text
+
+def test_not_filled_password(test_client):
+    data = {
+        "username": "username",
+        "password": ""
+    }
+
+    response = test_client.post('/', data=data)
+
+    assert response.status_code == 400
+    assert 'Vyplnte prihlásovacia údaje.' in response.text
